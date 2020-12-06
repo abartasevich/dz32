96fcd342b644a8f122aed34e61f8dbad10e204a9
Andrei <abartasevich@vtb.ru>
2020-12-06 09:11:30 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 3600;
+                int b = 2400;
+                int c = a/b;
+
+               System.out.println(c);
+    }
+}
